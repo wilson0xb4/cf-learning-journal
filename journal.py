2219a65203869e79786727c7023911ded7347ab8
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-06-25 15:03:30 -0700

    add add_entry and auth

--- journal.py
+++ journal.py
@@ -5,14 +5,18 @@ import datetime
 
 from pyramid.config import Configurator
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from waitress import serve
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+from cryptacular.bcrypt import BCRYPTPasswordManager
 
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 # make a module-level constant for the connection URI
@@ -55,6 +59,38 @@ def list_view(request):
     entries = Entry.all()
     return {'entries': entries}
 
+
+@view_config(route_name='add', request_method='POST')
+def add_entry(request):
+    title = request.params.get('title')
+    text = request.params.get('text')
+    Entry.write(title=title, text=text)
+    return HTTPFound(request.route_url('home'))
+
+
+@view_config(context=DBAPIError)
+def db_exception(context, request):
+    from pyramid.response import Response
+    response = Response(context.message)
+    response.status_int = 500
+    return response
+
+
+def do_login(request):
+    username = request.params.get('username', None)
+    password = request.params.get('password', None)
+    if not (username and password):
+        raise ValueError('both username and password are required')
+
+    settings = request.registry.settings
+
+    manager = BCRYPTPasswordManager()
+    if username == settings.get('auth.username', ''):
+        hashed = settings.get('auth.password', '')
+        return manager.check(hashed, password)
+    return False
+
+
 def main():
     """Create a configured wsgi app"""
     settings = {}
@@ -62,19 +98,32 @@ def main():
     settings['reload_all'] = debug
     settings['debug_all'] = debug
 
+    settings['auth.username'] = os.environ.get('AUTH_USERNAME', 'admin')
+    manager = BCRYPTPasswordManager()
+    settings['auth.password'] = os.environ.get(
+        'AUTH_PASSWORD', manager.encode('secret')
+    )
+
     if not os.environ.get('TESTING', False):
         # only bind the session if we are not testing
         engine = sa.create_engine(DATABASE_URL)
         DBSession.configure(bind=engine)
 
+    auth_secret = os.environ.get('JOURNAL_AUTH_SECRET', 'itsaseekrit')
+
     # configuration setup
     config = Configurator(
-        settings=settings
+        settings=settings,
+        authentication_policy=AuthTktAuthenticationPolicy(
+            secret=auth_secret,
+            hashalg='sha512'
+        ),
+        authorization_policy=ACLAuthorizationPolicy()
     )
     config.include('pyramid_tm')
     config.include('pyramid_jinja2')
     config.add_route('home', '/')
-    # config.add_route('other', '/other/{special_val}')
+    config.add_route('add', '/add')
     config.scan()
     app = config.make_wsgi_app()
     return app
