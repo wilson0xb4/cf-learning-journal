f3c0b0ae8190f4519faa8fad0b13e668e8be57fd
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-08 17:37:08 -0700

    date is now updated when an entry is edited. refs #14

--- journal.py
+++ journal.py
@@ -66,7 +66,7 @@ class Entry(Base):
         row.title = title
         row.text = text
         # breaks it? TODO: eventually figure out how to update the date
-        # row.created = datetime.datetime.utcnow
+        row.created = datetime.datetime.utcnow()
 
     @classmethod
     def all(cls, session=None):
